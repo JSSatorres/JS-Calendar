18b360c1bb7a1bfc8d7cf65d33bb4fa19497a1a0
Kory <50864697+Kory-ifcd0210@users.noreply.github.com>
2021-11-15 16:22:44 +0000

    edición aceptTask

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ function newTask (){
 // Crear la pantalla modal de newTask
 function createForm(){
     divEvent.innerHTML=
-    '<form id="event-content" action="">'+
+    '<form id="event-content" name="event-content">'+
     '<label>' + 'Name of Event'+
-        '<input type="text" id="nameEvent">'+'</label>'+
+        '<input type="text" id="nameEvent" name="nameEvent">'+'</label>'+
     '<label>'+'Date of Start'+
         '<input type="datetime" id="dateStart">'+'</label>'+
     '<label>'+
@@ -28,14 +28,14 @@ function createForm(){
         '<option value="1">'+'1 hour'+'</option>'+
     '</select>'+
             '<label>'+'Type of Event'+
-                '<select id="event" id="typeEvent">'+
+                '<select name="event" id="typeEvent">'+
                     '<option value="work">'+'work'+'</option>'+
                     '<option value="house">'+'house'+'</option>'+
                     '<option value="family">'+'family'+'</option>'+
                     '<option value="leisure">'+'leisure'+'</option>'+
                 '</select>'+'</label>'+
                 '<button class="col-left">'+'X'+'</button>'+
-                '<input type="button"  onclick="aceptTask()" value="Add">'+
+                '<input type="button" onclick="aceptTask()" value="Add">'+
     '</form>';
 }
 
